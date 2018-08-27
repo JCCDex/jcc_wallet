82119dd60a553b6de4c0289a2660ab8aec8f98f9
GinMu <gin.musheng@gmail.com>
2018-08-27 11:32:19 +0800

    version of 0.1.2

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const jcWallet = require('./jingchang');
+const callWallet = require('./call');
+const ethWallet = require('./eth');
+const stmWallet = require('./stm');
+
+exports = module.exports = {
+    jcWallet,
+    callWallet,
+    ethWallet,
+    stmWallet
+}
