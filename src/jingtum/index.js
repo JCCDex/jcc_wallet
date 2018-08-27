385bce3f3cdefd6c0a731601042ca754ec14854d
GinMu <gin.musheng@gmail.com>
2018-08-27 23:41:08 +0800

    added jingtum wallet tool and update jc wallet api

---
+++ src/jingtum/index.js
@@ -0,0 +1,50 @@
+const Wallet = require('jingtum-base-lib').Wallet;
+
+/**
+ * check jingtum address is valid or not
+ * @param {string} address
+ * @returns {boolean}
+ */
+const isValidAddress = (address) => {
+    return Wallet.isValidAddress(address);
+}
+
+/**
+ * check jingtum secret is valid or not
+ * @param {string} secret
+ * @returns {boolean}
+ */
+const isValidSecret = (secret) => {
+    return Wallet.isValidSecret(secret);
+}
+
+/**
+ * get address through decrypting secret
+ * @param {string} secret
+ * @returns {string | null} return address if success, otherwise return null
+ */
+const getAddress = (secret) => {
+    try {
+        let wallet = Wallet.fromSecret(secret);
+        return wallet.address
+    } catch (error) {
+        return null;
+    }
+}
+
+/**
+ * create jingtum wallet
+ * @param {*} opt
+ * @returns {object} { address: '', secret: '' }
+ */
+const createWallet = () => {
+    let wallet = Wallet.generate();
+    return wallet;
+}
+
+module.exports = {
+    isValidAddress,
+    isValidSecret,
+    getAddress,
+    createWallet
+}
---
+++ test/test_jingtum.js
@@ -0,0 +1,58 @@
+const chai = require('chai');
+const expect = chai.expect;
+const jtWallet = require('../src/jingtum');
+let testAddress = 'jpgWGpfHz8GxqUjz5nb6ej8eZJQtiF6KhH';
+let testSecret = 'snfXQMEVbbZng84CcfdKDASFRi4Hf'
+describe('test jingtum', function () {
+
+    describe('test isValidAddress', function () {
+        it('should return true when the jingtum address is valid', function () {
+            let isValid = jtWallet.isValidAddress(testAddress)
+            expect(isValid).to.equal(true);
+        })
+
+        it('should return false when the jingtum address is not valid', function () {
+            let isValid = jtWallet.isValidAddress(testAddress.substring(1))
+            expect(isValid).to.equal(false);
+        })
+    })
+
+    describe('test isValidSecret', function () {
+        it('should return true when the jingtum secret is valid', function () {
+            let isValid = jtWallet.isValidSecret(testSecret);
+            expect(isValid).to.equal(true);
+        })
+
+        it('should return false when the jingtum secret is not valid', function () {
+            let isValid = jtWallet.isValidSecret(testSecret.substring(1));
+            expect(isValid).to.equal(false);
+        })
+    })
+
+    describe('test getAddress', function () {
+        it('should return correct address if the secret is valid', function () {
+            let address = jtWallet.getAddress(testSecret);
+            expect(address).to.equal(testAddress);
+        })
+
+        it('should return null if the secret is not valid', function () {
+            let address = jtWallet.getAddress(testSecret.substring(1));
+            expect(address).to.equal(null);
+        })
+    })
+
+    describe('test createWallet', function () {
+        it('the wallet should be valid when create jingtum wallet successfully', function () {
+            let wallet = jtWallet.createWallet();
+            let {
+                secret,
+                address
+            } = wallet;
+            let a = jtWallet.isValidAddress(address);
+            let b = jtWallet.isValidSecret(secret);
+            expect(a).to.equal(true);
+            expect(b).to.equal(true);
+        })
+    })
+
+});
