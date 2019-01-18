78c3910c7f37023f26809909b677bafab0d7ee88
GinMu <gin.musheng@gmail.com>
2019-01-18 15:10:45 +0800

    Fixed spelling error

--- src/jingtum/index.js
+++ src/jingtum/index.js
@@ -3,32 +3,32 @@ const Wallet = require('jcc_jingtum_base_lib').Wallet;
 /**
  * check jingtum address is valid or not
  * @param {string} address
- * @param {string} chian
+ * @param {string} chain
  * @returns {boolean}
  */
-const isValidAddress = (address, chian = 'swt') => {
-    return Wallet.isValidAddress(address, chian);
+const isValidAddress = (address, chain = 'swt') => {
+    return Wallet.isValidAddress(address, chain);
 }
 
 /**
  * check jingtum secret is valid or not
  * @param {string} secret
- * @param {string} chian
+ * @param {string} chain
  * @returns {boolean}
  */
-const isValidSecret = (secret, chian = 'swt') => {
-    return Wallet.isValidSecret(secret, chian);
+const isValidSecret = (secret, chain = 'swt') => {
+    return Wallet.isValidSecret(secret, chain);
 }
 
 /**
  * get address through decrypting secret
  * @param {string} secret
- * @param {string} chian
+ * @param {string} chain
  * @returns {string | null} return address if success, otherwise return null
  */
-const getAddress = (secret, chian = 'swt') => {
+const getAddress = (secret, chain = 'swt') => {
     try {
-        let wallet = Wallet.fromSecret(secret, chian);
+        let wallet = Wallet.fromSecret(secret, chain);
         return wallet.address
     } catch (error) {
         return null;
@@ -37,11 +37,11 @@ const getAddress = (secret, chian = 'swt') => {
 
 /**
  * create jingtum wallet
- * @param {string} chian
+ * @param {string} chain
  * @returns {object} { address: '', secret: '' }
  */
-const createWallet = (chian = 'swt') => {
-    let wallet = Wallet.generate(chian);
+const createWallet = (chain = 'swt') => {
+    let wallet = Wallet.generate(chain);
     return wallet;
 }
 
