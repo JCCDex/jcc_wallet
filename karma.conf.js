d1e644d685a89431f28cfaafc19b2832644d06fb
GinMu <gin.musheng@gmail.com>
2019-03-26 15:08:08 +0800

    Test in browser with karma

---
+++ karma.conf.js
@@ -0,0 +1,49 @@
+module.exports = function (config) {
+    config.set({
+        frameworks: ["browserify", "detectBrowsers", "mocha"],
+        files: [
+            "test/test_*.js"
+        ],
+        preprocessors: {
+            // don't use browserify, because it don't support dynamic require.
+            // and some file is dynamic required in the `stm-lib` package.
+            "test/test_*.js": ["webpack"]
+        },
+        singleRun: true,
+        plugins: [
+            "karma-webpack",
+            "karma-browserify",
+            "karma-chrome-launcher",
+            "karma-env-preprocessor",
+            "karma-firefox-launcher",
+            "karma-detect-browsers",
+            "karma-mocha"
+        ],
+        webpack: {
+            node: {
+                fs: "empty",
+                tls: "empty",
+                "child_process": "empty",
+                net: "empty"
+            },
+            mode: "production"
+        },
+        envPreprocessor: [
+            "RANDOM_TESTS_REPEAT"
+        ],
+        detectBrowsers: {
+            enabled: true,
+            usePhantomJS: false,
+            postDetection(availableBrowser) {
+                if (availableBrowser.includes("Chrome")) {
+                    return ["ChromeHeadless"];
+                }
+
+                var browsers = ["Chrome", "Firefox"];
+                return browsers.filter(function (browser) {
+                    return availableBrowser.indexOf(browser) !== -1;
+                });
+            }
+        }
+    });
+};
