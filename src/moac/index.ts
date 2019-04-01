ac10545aa1e1c9093074e55886f4c76d811df078
GinMu <gin.musheng@gmail.com>
2019-04-01 10:23:39 +0800

    Imported enc-hex、sha3 & ec in demand

--- src/moac/index.ts
+++ src/moac/index.ts
@@ -1,11 +1,11 @@
 "use strict";
 
 import moacUtils = require("chain3/lib/utils/utils");
-import CryptoJS = require("crypto-js");
-import { ec } from "elliptic";
+import Hex = require("crypto-js/enc-hex");
+import Sha3 = require("crypto-js/sha3");
+import ec = require("elliptic/lib/elliptic/ec");
 import ethereumjsUtil = require("ethereumjs-util");
 import { filterOx } from "jcc_common";
-const EC = new ec("secp256k1");
 
 /**
  * check moac secret is valid or not
@@ -39,15 +39,16 @@ const isValidAddress = (address: string): boolean => {
  */
 const getAddress = (secret: string): string | null => {
     if (isValidSecret(secret)) {
+        const EC = new ec("secp256k1");
         const keyPair = EC.genKeyPair();
         keyPair._importPrivate(filterOx(secret), "hex");
         const compact = false;
         const pubKey = keyPair.getPublic(compact, "hex").slice(2);
-        const pubKeyWordArray = CryptoJS.enc.Hex.parse(pubKey);
-        const hash = CryptoJS.SHA3(pubKeyWordArray, {
+        const pubKeyWordArray = Hex.parse(pubKey);
+        const hash = Sha3(pubKeyWordArray, {
             outputLength: 256
         });
-        const address = hash.toString(CryptoJS.enc.Hex).slice(24);
+        const address = hash.toString(Hex).slice(24);
         return "0x" + address;
     }
     return null;
