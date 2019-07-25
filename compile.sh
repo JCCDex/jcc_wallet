e7b8a5be8d8679b5c92ac15ae803b2a699ebe7fa
GinMu <gin.musheng@gmail.com>
2019-07-25 09:40:58 +0800

    update compile.sh

--- compile.sh
+++ compile.sh
@@ -7,6 +7,7 @@ npx babel ./node_modules/swtc-factory --out-dir ./node_modules/swtc-factory
 npx babel ./node_modules/swtc-keypairs --out-dir ./node_modules/swtc-keypairs
 npx babel ./node_modules/swtc-chains --out-dir ./node_modules/swtc-chains
 npx babel ./node_modules/swtc-address-codec --out-dir ./node_modules/swtc-address-codec
+npx babel ./node_modules/scryptsy --out-dir ./node_modules/scryptsy
 
 ./node_modules/cross-env/dist/bin/cross-env-shell.js MODE=$1 REPORT=$2 webpack
 
